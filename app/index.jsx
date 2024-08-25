6c32e95587199c2e14e3f2c4691b15ffba578466
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-25 22:27:55 +0530

    initials

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,17 @@
-import { View, Text, StyleSheet } from "react-native"
-
+import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>HI</Text>
     </View>
-   
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
-
