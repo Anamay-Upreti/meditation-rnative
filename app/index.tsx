893493c58422f412c38d616e7141803712a77aa6
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-26 23:02:55 +0530

    adding-components

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,37 @@
-import { View, Text, ImageBackground } from 'react-native';
-import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
-import beachImage from '@/assets/meditation-images/beach.webp';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { View, Text, ImageBackground, } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <ImageBackground source={beachImage} resizeMode="cover" style={{ flex: 1 }}>
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        style={{ flex: 1 }}
+      >
         <LinearGradient
           style={{ flex: 1 }}
           colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
-        > <SafeAreaView>
-            <Text>App</Text>
-        </SafeAreaView>
-         
+        >
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+            <Text className="text-center text-white font-bold text-4xl">
+              Simple Meditation
+            </Text>
+            <Text className="text-center text-white text-regular text-2xl mt-3">
+              Simplifying Meditation For Everyone
+            </Text>
+            </View>
+
+            <View>
+              
+            </View> 
+
+            <StatusBar style="light"/>
+          </SafeAreaView>
         </LinearGradient>
       </ImageBackground>
     </View>
