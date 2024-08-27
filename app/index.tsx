c95767adc8d72e5432193a014ded09063bf1406d
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-27 23:13:04 +0530

    added-use-router

--- app/index.tsx
+++ app/index.tsx
@@ -6,8 +6,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import CustomButton from "@/components/CustomButton";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useRouter } from "expo-router";
 
 const App = () => {
+  const router = useRouter();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
@@ -31,7 +33,7 @@ const App = () => {
               </View>
 
               <View>
-                <CustomButton onPress={() => console.log('tap')} title="Get Started" />
+                <CustomButton onPress={() => router.push("/test")} title="Get Started" />
               </View>
 
               <StatusBar style="light" />
