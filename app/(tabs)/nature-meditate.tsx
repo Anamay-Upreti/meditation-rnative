5e922b9c3317091fea1fe814a9f5c2caba3e0200
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-28 22:47:32 +0530

    started-nature-section

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import AppGradient from '@/components/AppGradient'
 
 const natureMeditate = () => {
   return (
-    <View>
+    <View className='flex-1'>
+      <AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
       <Text>nature-meditate</Text>
+      </AppGradient>
+     
     </View>
   )
 }
